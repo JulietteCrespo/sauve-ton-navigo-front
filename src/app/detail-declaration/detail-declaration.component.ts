195e6f3d9aa465e654102fe104c44815fb053270
jcrespo <juliette.crespo@epfedu.fr>
2023-11-22 15:25:53 +0100

    debut detail

--- src/app/detail-declaration/detail-declaration.component.ts
+++ src/app/detail-declaration/detail-declaration.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-detail-declaration',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-declaration.component.scss']
 })
 export class DetailDeclarationComponent {
+  constructor(private route: ActivatedRoute) {}
 
+
+  changerPhoto(variable:number): string {
+    const numeroMaxLignes = 14;
+    const numeroLigne = Math.min(Math.max(variable, 1), numeroMaxLignes);
+    const cheminImage = `../../assets/images/ligne${numeroLigne}.svg`;
+    return cheminImage;
+  }
 }
